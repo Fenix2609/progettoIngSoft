093663c9f9356a31cb511d7c12d3f341d2541d6c
ivang <ivangrande2609@gmail.com>
2025-08-22 12:40:51 +0200

    secondo commit

--- src/main/java/controller/LibreriaController.java
+++ src/main/java/controller/LibreriaController.java
@@ -1,4 +1,50 @@
 package controller;
 
+import model.Libro;
+import persistence.PersistenceManager;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LibreriaController {
+    private List<Libro> libri;
+    private PersistenceManager persistence;
+    private static LibreriaController instance;
+    private LibreriaController(PersistenceManager persistence) {
+        this.libri = new ArrayList<>();
+        this.persistence = persistence;
+    }
+
+    // Metodo per ottenere l’unica istanza (Singleton)
+    public static LibreriaController getInstance(PersistenceManager persistence) {
+        if (instance == null) {
+            instance = new LibreriaController(persistence);
+        }
+        return instance;
+    }
+
+    public void aggiungiLibro(Libro libro) { libri.add(libro); }
+    public void rimuoviLibro(String isbn) { libri.removeIf(b -> b.getIsbn().equals(isbn)); }
+
+    public List<Libro> getLibri() { return libri; }
+
+    public List<Libro> cercaDalTitolo(String titolo) {
+        return libri.stream().filter(b -> b.getTitolo().toLowerCase().contains(titolo.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Libro> filtraPerGenere(String genere) {
+        return libri.stream().filter(b -> b.getGenere().equalsIgnoreCase(genere))
+                .collect(Collectors.toList());
+    }
+
+    public List<Libro> sortByAuthor() {
+        return libri.stream().sorted(Comparator.comparing(Libro::getAutore))
+                .collect(Collectors.toList());
+    }
+
+    public void save(String nomeFile) { persistence.save(nomeFile, libri); }
+    public void load(String nomeFile) { libri = persistence.load(nomeFile); }
 }
